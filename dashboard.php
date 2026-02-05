4522af0f1c5db4e2671112d34a8aa0bd2d582818
NuhmanXas <xas.repository@gmail.com>
2026-02-05 14:54:35 +0530

    2026-2-5 final

--- dashboard.php
+++ dashboard.php
@@ -18,13 +18,31 @@ include './layout/PageHead.php';
 ?>
 
 <link rel="stylesheet" href="./dashboard.css">
+<!-- Basic Icons -->
+<link href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css" rel="stylesheet">
+<!-- Filled Icons -->
+<link href="https://cdn.boxicons.com/3.0.8/fonts/filled/boxicons-filled.min.css" rel="stylesheet">
+<!-- Brand Icons -->
+<link href="https://cdn.boxicons.com/3.0.8/fonts/brands/boxicons-brands.min.css" rel="stylesheet">
 
 <div class="container-fluid">
     <div class="row vh-100">
         <div class="col-lg-3 border sidebar-container">
             <div class="row text-white">
                 <div class="col-lg-12 d-flex justify-content-center align-items-center logo-container">HND SE 25</div>
-                <div class="col-lg-12 navigation-container"> Navigation Area</div>
+                <div class="col-lg-12 navigation-container"> 
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link text-white active" href="#"> <i class="bx bx-dashboard" ></i>  Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#"> <i class="bx bx-user" ></i> Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#"> <i class="bx bx-cog" ></i> Settings</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
         <div class="col-lg-9 border main-container">
