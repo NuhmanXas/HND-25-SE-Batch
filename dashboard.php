a9b2409241cd43643e84b437854ad94f270fde88
NuhmanXas <xas.repository@gmail.com>
2026-02-05 14:26:52 +0530

    2026-2-5 1

--- dashboard.php
+++ dashboard.php
@@ -17,10 +17,17 @@ include './services/databaseConfig.php';
 include './layout/PageHead.php';
 ?>
 
+<link rel="stylesheet" href="./dashboard.css">
+
 <div class="container-fluid">
     <div class="row vh-100">
-        <div class="col-lg-3 border ">SideBar</div>
-        <div class="col-lg-9 border">
+        <div class="col-lg-3 border sidebar-container">
+            <div class="row text-white">
+                <div class="col-lg-12 d-flex justify-content-center align-items-center logo-container">HND SE 25</div>
+                <div class="col-lg-12 navigation-container"> Navigation Area</div>
+            </div>
+        </div>
+        <div class="col-lg-9 border main-container">
             <div class="row">
                 <div class="col-lg-12 border d-flex justify-content-between align-items-center">
                     <h3>Dashboard</h3>
